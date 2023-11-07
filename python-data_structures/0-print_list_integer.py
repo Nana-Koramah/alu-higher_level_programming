1f2b28732db47060b529f1bb6d78652f56065045
Nana-Koramah <n.abeasi@alustudent.com>
2023-11-07 07:06:54 -0800

    0 commit

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
+# This code lists all the integers in a list.
+
+
 def print_list_integer(my_list=[]):
     """Print all integers of a list."""
-for i in range(len(my_list)):
-    print("{:d}".format(my_list[i]))
+    for i in range(len(my_list)):
+        print("{:d}".format(my_list[i]))
 
