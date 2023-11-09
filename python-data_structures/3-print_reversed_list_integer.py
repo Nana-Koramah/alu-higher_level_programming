9a2a0dee41755852b790f390d74087daaee0fa89
Nana-Koramah <n.abeasi@alustudent.com>
2023-11-09 07:51:27 -0800

    3 commit

---
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+# This is a function that prints all integers of a list in reverse.
+def print_reversed_list_integer(my_list=[]):
+    for integer in my_list:
+        print(int(my_list[-1:]))
