7e4c1aba79613b4d4bfda3e2581d7e015464a11c
Nana-Koramah <n.abeasi@alustudent.com>
2023-11-09 09:05:36 -0800

    4 commit

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -3,9 +3,11 @@
 
 
 def new_in_list(my_list, idx, element):
-         if idx < 0 or idx > (len(my_list) - 1):
-        return (my_list)
-
-    copy = [x for x in my_list]
-    copy[idx] = element
-    return (copy)
+    if idx < 0:
+        if idx > (len(my_list) - 1):
+            return (my_list)
+        else:
+            copy = x
+            for x in my_list:
+                copy[idx] = element
+                return copy
