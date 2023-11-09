64a1e4d8af5e9dc392417305da1439bf0798cc4e
Nana-Koramah <n.abeasi@alustudent.com>
2023-11-09 11:28:52 -0800

    4 commit

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
-# This is a function that replaces an element in a list without changing original list.
+# This is a function that replaces an element in a list without changing original
 
 
 def new_in_list(my_list, idx, element):
-    if idx < 0:
-        if idx >= len(my_list):
-              return my_list.copy()
+    if idx < 0 or idx >= len(my_list):
+            return my_list.copy()
         else:
             new_list = my_list.copy()
             new_list[idx] = element
