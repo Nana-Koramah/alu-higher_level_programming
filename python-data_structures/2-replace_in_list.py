81d1da00a55f9378749ca36a92620c399ca32bc7
Nana-Koramah <n.abeasi@alustudent.com>
2023-11-09 07:38:02 -0800

    2 commit

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -4,6 +4,6 @@
 
 def replace_in_list(my_list, idx, element):
     if idx >= 0:
-        if idx < (len(my_list)):
+        if idx <= (len(my_list)):
             my_list[idx] = element
         return my_list
