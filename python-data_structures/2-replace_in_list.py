58d723f16f25ffe46aef887e014a583903abb060
Nana-Koramah <n.abeasi@alustudent.com>
2023-11-09 07:23:48 -0800

    2 commit

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 # This code is a function that replaces an element of a list at a specific postion.
 def replace_in_list(my_list, idx, element):
-    if not (idx < 0 or idx > (len(my_list) - 1)):
-        my_list[idx] = element
-        return (my_list)
+    if idx >= 0:
+        if idx < (len(my_list)):
+            my_list[idx] = element
+            return my_list
