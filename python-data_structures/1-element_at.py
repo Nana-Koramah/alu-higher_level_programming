559811f37daf3879d665bbb755850f0d31160aa4
Nana-Koramah <n.abeasi@alustudent.com>
2023-11-09 06:40:48 -0800

    1 commit

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 # This code retrieves an element from a list
 def element_at(my_list, idx):
-   if idx < 0 or idx > (len(my_list) - 1):
-       return None
-   else:
-       return (my_list[idx])
+if idx < 0 or idx > (len(my_list) - 1):
+    return None
+else:
+    return (my_list[idx])
