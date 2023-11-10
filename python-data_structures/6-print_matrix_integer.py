286dd75fa01625cb96f9caed80a441b679d4e1d2
Nana-Koramah <n.abeasi@alustudent.com>
2023-11-10 04:02:36 -0800

    6 commit

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -6,7 +6,7 @@ def print_matrix_integer(matrix=[[]]):
     matrix = []
     for row in matrix:
         for col in row:
-        print("{:d}".format(col), end="")
+            print("{:d}".format(col), end="")
         if col != row[-1]:
             else:
                 print()
