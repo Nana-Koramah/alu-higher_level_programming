34406c438cf512175c4c0cac3206db08d5fa5fd5
Nana-Koramah <n.abeasi@alustudent.com>
2023-11-10 05:01:55 -0800

    9 commit

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -3,6 +3,6 @@
 
 
 def multiple_returns(sentence):
-     if sentence == "":
+    if sentence == "":
         return (0, None)
-     return (len(sentence), sentence[0])
+    return (len(sentence), sentence[0])
