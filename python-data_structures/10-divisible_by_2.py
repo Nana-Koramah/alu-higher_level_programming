9f5fe242f022997c7129bfdd5c6a193716cb4374
Nana-Koramah <n.abeasi@alustudent.com>
2023-11-10 05:38:04 -0800

    10 commit

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -4,10 +4,9 @@
 
 def divisible_by_2(my_list=[]):
     new_list = []
-    for x in range(len(my_list)):
-        if x/2 == 0:
+    for i in range(len(my_list)):
+        if my_list[i]%2 == 0:
             new_list.append(True)
-            print("x is divisible by 2")
         else:
             new_list.append(False)
-            print("x is not divisible by 2")
+    return new_list       
