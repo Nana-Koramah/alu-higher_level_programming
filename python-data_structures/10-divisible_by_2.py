03539c140330444de1c74703e994a506f6668e6a
Nana-Koramah <n.abeasi@alustudent.com>
2023-11-10 05:11:16 -0800

    9 commit

---
+++ python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# a function that finds all multiples of 2 in a list.
+
+
+def divisible_by_2(my_list=[]):
+    x = my_list
+    for x in my_list:
+        if x/2:
+            print("x is divisible by 2")
+        else:
+            print("x is not divisible by 2")
