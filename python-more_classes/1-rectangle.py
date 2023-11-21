19e32f43a6d2a2e8a727751bf8d650d5695b9ab6
Nana-Koramah <n.abeasi@alustudent.com>
2023-11-21 09:07:24 -0800

    1 commit

---
+++ python-more_classes/1-rectangle.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""Define a square"""
+
+
+class Rectangle():
+    """Represent a square"""
+
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+        """initialize a new square"""
+
+
+    @property
+    def width(self):
+        """retrieve the width of the square"""
+
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Set the width of the square"""
+
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """retrieve the height of the square"""
+
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
