d67b5302cb7cc176762215348fb181858a26085f
Nana-Koramah <n.abeasi@alustudent.com>
2023-12-01 12:24:17 -0800

    update

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -4,11 +4,12 @@ fetch https://alu-intranet.hbtn.io/status; display response
 """
 
 import urllib.request
+from contextlib import closing
 
 url = "https://alu-intranet.hbtn.io/status"
 
 try:
-    with urllib.request.urlopen(url) as response:
+    with closing(urllib.request.urlopen(url)) as response:
         body = response.read().decode('utf-8')
         print(f"Body response:\n\t- type: {type(body)}\n\t- content: {body}")
 except urllib.error.HTTPError as e:
