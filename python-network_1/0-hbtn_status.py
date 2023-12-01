b714f839145e0ede9d01807a2ca03d579006ec17
Nana-Koramah <n.abeasi@alustudent.com>
2023-12-01 12:31:02 -0800

    update

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -5,15 +5,35 @@ fetch https://alu-intranet.hbtn.io/status; display response
 
 import urllib.request
 
-url = "https://alu-intranet.hbtn.io/status"
+def fetch_status(url):
+    """
+    Fetches the status from the specified URL using urllib.
 
-try:
-    with urllib.request.urlopen(url) as response:
-        body = response.read().decode('utf-8')
-        print("Body response:")
-        print(f"\t- type: {type(body)}")
-        print(f"\t- content: {body}")
-except urllib.error.HTTPError as e:
-    print(f"Error code: {e.code}")
-except urllib.error.URLError as e:
-    print(f"Error: {e.reason}")
+    Args:
+        url (str): The URL to fetch the status from.
+
+    Returns:
+        None: The function prints the body response or error information.
+    """
+    try:
+        # Open the URL using urllib and create a context manager with 'with'
+        with urllib.request.urlopen(url) as response:
+            # Read the content of the response and decode it
+            body = response.read().decode('utf-8')
+            
+            # Display the body response information
+            print("Body response:")
+            print(f"\t- type: {type(body)}")
+            print(f"\t- content: {body}")
+
+    except urllib.error.HTTPError as e:
+        # Handle HTTP errors
+        print(f"Error code: {e.code}")
+
+    except urllib.error.URLError as e:
+        # Handle URL errors
+        print(f"Error: {e.reason}")
+
+# Example usage
+url = "https://alu-intranet.hbtn.io/status"
+fetch_status(url)
